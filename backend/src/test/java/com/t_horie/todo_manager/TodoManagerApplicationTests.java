210bc97a3ca308cda44e935352f79bf8a221a6a7
HORIE Tetsuya <horie-t@users.noreply.github.com>
2025-11-16 12:03:12 +0900

    バックエンドのSpring Bootプロジェクトを作成する

---
+++ backend/src/test/java/com/t_horie/todo_manager/TodoManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t_horie.todo_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
