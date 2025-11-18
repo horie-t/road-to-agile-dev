1643cd947e28c093db44a0f1120d9374e9b660e0
HORIE Tetsuya <horie-t@users.noreply.github.com>
2025-11-18 12:22:05 +0900

    フロントエンドに単体テストを追加する

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { configDefaults } from 'vitest/config'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: [],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html'],
+    },
+    exclude: [...configDefaults.exclude],
+  },
 })
